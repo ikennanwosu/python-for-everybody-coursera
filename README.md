20f6401d3b6002fbd13570d2f16e6036511d053b
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-01-05 20:51:00 +0800

    Add README, .gitignore, and some more code
    Signed-off-by: Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python for Everybody
+Code written while doing the coursera specialization: [Python for Everybody](https://www.coursera.org/specializations/python).
---
+++ course3/w3/main.py
@@ -0,0 +1,13 @@
+import urllib
+import re
+
+fname = "http://py4e-data.dr-chuck.net/regex_sum_56359.txt"
+datasource = urllib.urlopen(fname)
+line = datasource.readline()
+total_sum = 0
+while line != "":
+	number_sum_in_this_line = sum(map(int, re.findall('[0-9]+', line)))
+	total_sum += number_sum_in_this_line
+	line = datasource.readline()
+
+print total_sum
